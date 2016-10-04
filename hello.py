0e0723c441565ccc08a04163b2ba0800c4b0c6aa
MINDAUGAS VAINAUSKAS - Student <G00327967@gmit.ie>
2016-10-04 15:18:04 +0100

    Bootstrap basic template downloaded, JS and CSS links fixed and entire page rerun on the browser

--- hello.py
+++ hello.py
@@ -4,6 +4,10 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello World!"
+	
+@app.route("/name")
+def name():
+    return "name goes here"
 
 if __name__ == "__main__":
     app.run()
